02184a41b694d17be18c6a61a1e774f123b2959c
Louise Collins <louisecollins1971@icloud.com>
2021-08-26 16:37:09 +0100

    14.Time.deltaTime tut  complete

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
-{   
-    // Start is called before the first frame update
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update.
     void Start()
-    {
+    {   
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
-        transform.Translate(xValue,0, zValue);
-    }   
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
 } 

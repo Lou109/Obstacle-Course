688b5cbcb269896a8918e3c3d4475b05ec72e8c5
Louise Collins <louisecollins1971@icloud.com>
2023-12-15 18:05:16 +0000

    C# Formatting & Input.GetAxis()

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
-{
-    [SerializeField]float xValue = 0;
-    [SerializeField]float yValue = 0.01f;
-    [SerializeField]float zValue = 0;
 
-    // Start is called before the first frame update
+{
     void Start()
     {
        
     }
 
-    // Update is called once per frame
     void Update()
     {
-         transform.Translate(xValue,yValue,zValue);
+         float xValue = Input.GetAxis("Horizontal");
+         float zValue = Input.GetAxis("Vertical");
+         transform.Translate(xValue,0,zValue);
     }
 }

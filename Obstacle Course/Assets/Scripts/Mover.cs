665114a2cdef4d0d566af8321ef69598ecbc8c88
Louise Collins <louisecollins1971@icloud.com>
2023-12-16 14:08:17 +0000

    Added  a Jump mechanic to  the player object

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -7,10 +8,13 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float jumpForce = 300f;
+    Rigidbody rb;
+    bool playerIsOnTheGround = true;
      
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
@@ -28,5 +32,21 @@ public class Mover : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation (movementDirection, Vector3.up);
             transform.rotation = Quaternion. RotateTowards (transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && playerIsOnTheGround == true)
+        {
+           rb.AddForce(Vector3.up * jumpForce);
+           playerIsOnTheGround = false;
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    
+    {
+        if(collision.gameObject.tag == "Ground")
+        
+        {
+            playerIsOnTheGround = true;
+        }
+    }   
 }

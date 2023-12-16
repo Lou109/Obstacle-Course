67518fc46742260df4f22baeb1aa9f0546dcc261
Louise Collins <louisecollins1971@icloud.com>
2023-12-16 01:31:39 +0000

    Time.deltaTime plus added rotation in the direction of movement for the character

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 
 {
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float rotationSpeed;
+     
     void Start()
     {
        
     }
-
     void Update()
     {
-         float xValue = Input.GetAxis("Horizontal");
-         float zValue = Input.GetAxis("Vertical");
-         transform.Translate(xValue,0,zValue);
+        float xValue = Input.GetAxis ("Horizontal");
+        float zValue = Input.GetAxis ("Vertical");
+
+        Vector3 movementDirection = new Vector3 (xValue, 0 , zValue);
+        movementDirection.Normalize();
+
+        transform.Translate (movementDirection * moveSpeed * Time.deltaTime, Space.World); 
+
+        if (movementDirection != Vector3.zero)
+
+        {
+            Quaternion toRotation = Quaternion.LookRotation (movementDirection, Vector3.up);
+            transform.rotation = Quaternion. RotateTowards (transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
